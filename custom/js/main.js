c33dd23f8f6a95caf6a435cca97fbd21b731d600
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-11-29 02:51:18 +0700

    aaaaaaa

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,23 +1,32 @@
 var session_key = "";
 var username = "";
 
-function onclick_login(code) {
-	var postFormStr = "<form method='POST' action='" + "/./html/index.html" + "'>\n";
-	postFormStr += "<input type='hidden' name='" + "haha" + "' value='" + "hihi" + "'></input>";
-	postFormStr += "</form>";
-	// alert(postFormStr);
-	// var str = "Vương";
-	// var strp = $(str);
-	// alert(strp);
-	var formElement = $(postFormStr);
-	$('body').append(formElement);
-	$(formElement).submit();
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function onclick_redirect(dest) {
-	var postFormStr = "<form method='POST' action='" + "/./" + dest + "'>\n";
-	postFormStr += "</form>";
-	var formElement = $(postFormStr);
-	$('body').append(formElement);
-	$(formElement).submit();
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function onclick_redirect(dest, code="") {
+	$form = $("<form method='POST' action='register.html'></form>");
+    $form.append("<input type=haha value=hihi></input>");
+    $('body').append(formElement);
+    $(formElement).submit();
 }
